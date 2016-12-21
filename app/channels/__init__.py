3e5e311d1e5c9f73a892d74ce997af6a66a8faee
c-schwarze <ces55739@bethel.edu>
2016-12-21 14:33:42 -0600

    Added basic failed channel rendering

--- app/channels/__init__.py
+++ app/channels/__init__.py
@@ -4,10 +4,13 @@ from app.channels.channel_base import *
 
 
 def render_channel(class_name):
-    module_name = '_'.join(re.findall('[A-Z][^A-Z]*', class_name)).lower()
-    module = importlib.import_module('app.channels.' + module_name + '.' + module_name)
+    try:
+        module_name = '_'.join(re.findall('[A-Z][^A-Z]*', class_name)).lower()
+        module = importlib.import_module('app.channels.' + module_name + '.' + module_name)
+        return getattr(module, class_name)().render()
+    except:
+        return 'Channel failed to load'
 
-    return getattr(module, class_name)().render()
 
 # def render_channel(class_name, channel_options_table):
 #     module_name = '_'.join(re.findall('[A-Z][^A-Z]*', class_name)).lower()
